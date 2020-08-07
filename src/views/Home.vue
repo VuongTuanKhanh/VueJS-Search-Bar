e168271469e3aaac6f9589844ee2261d62a4e089
Archius Vương <khanhvuongtuan37@gmail.com>
2020-08-07 11:17:31 +0700

    Add Search and List section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    Hello
+  <div class="container">
+    <Search class="search" />
+    <List class="list" />
   </div>
 </template>
 
 <script>
-export default {};
+import Search from "@/components/Search.vue";
+import List from "@/components/List.vue";
+export default {
+  name: "Home",
+  components: {
+    Search,
+    List
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.search {
+  width: 30%;
+  height: 100%;
+  position: relative;
+  border-right: 1px solid gray;
+}
+
+.list {
+  width: 70%;
+  height: 100%;
+  position: relative;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+}
+</style>
